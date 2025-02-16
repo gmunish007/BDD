fd434e158a545bfc8c57ce61557032139a394537
gmunish <gmunish@rocketmail.com>
2025-02-17 01:02:57 +0530

    message1

--- src/test/java/stepDefinition/SampleFile.java
+++ src/test/java/stepDefinition/SampleFile.java
@@ -5,6 +5,14 @@ public class SampleFile {
 	public static void main(String[] args) {
 		
 		System.out.println("3");
+		SampleFile obj = new SampleFile();
+		int demo = obj.sumMethod(4, 5);
+		System.out.println(demo);
+		
+	}
+	
+	public int sumMethod(int a, int b) {
+		return a+b;
 	}
 
 }
